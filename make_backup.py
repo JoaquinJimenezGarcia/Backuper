b97fc6eeb73301d3f9c06deb2362bf1703cec595
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2020-11-30 23:45:24 +0100

    Added option to encrypt file

--- make_backup.py
+++ make_backup.py
@@ -1,11 +1,17 @@
 import os
 import yaml
+import pyminizip
+import datetime
 
 with open("config.yaml", "r") as f:
     config = yaml.load(f)
 
-backup_folder = " backup.$(date +%Y%m%d)" 
+x = datetime.datetime.now()
+backup_folder = " backup."+x.strftime("%Y")+x.strftime("%m")+x.strftime("%d")
+backup_folder_zip = "backup."+x.strftime("%Y")+x.strftime("%m")+x.strftime("%d") + ".zip"
+backup_folder_encrypted = backup_folder_zip + ".encr"
 backup_destiny = config["destiny"]
+compression_level = 5
 
 os.system("mkdir " + backup_folder)
 
@@ -14,6 +20,18 @@ for path in config["paths"]:
     os.system("cp -r " + path + backup_folder)
 
 print("Compressing backup...")
-os.system("zip -r" +  backup_folder + ".zip" + backup_folder)
-print("Moving backup to " + backup_destiny)
-os.system("mv" + backup_folder + ".zip " + backup_destiny)
+os.system("zip -r " +  backup_folder_zip + backup_folder)
+
+if config["encrypted"]:
+    print("Encrypting backup file...")
+    pyminizip.compress(backup_folder_zip, "", backup_folder_encrypted, config["encrypting_password"], compression_level)
+
+    print("Moving backup to " + backup_destiny)
+    os.system("mv " + backup_folder_encrypted + " " + backup_destiny)
+    os.system("rm -rf " + backup_folder_zip)
+
+else:    
+    print("Moving backup to " + backup_destiny)
+    os.system("mv" + backup_folder + ".zip " + backup_destiny)
+
+os.system("rm -rf" + backup_folder)
